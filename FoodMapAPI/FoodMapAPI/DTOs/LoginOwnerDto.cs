f77190577576adc1416e047269a08b17c6b828ec
Nguyen Quoc Minh <quocminhnm123456789@gmail.com>
2026-06-27 01:47:04 +0700

    Xoa cau truc cu da duoc thay bang backend/frontend

--- FoodMapAPI/FoodMapAPI/DTOs/LoginOwnerDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FoodMapAPI.DTOs
-{
-    public class LoginOwnerDto
-    {
-        public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
